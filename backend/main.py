1e7df85e5912e857e66d49a04a3fad56db41db52
Pranav Jayachandran <106595992+PranavJ6210@users.noreply.github.com>
2025-12-18 20:49:56 +0530

    Update main.py cors

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://barani-portfolio.onrender.com/contact","https://barani-portfolio.onrender.com/services-rfq","https://barani-portfolio.onrender.com/careers"],  # In production, specify the frontend domain
+    allow_origins=["https://barani-portfolio.onrender.com"],  # In production, specify the frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
